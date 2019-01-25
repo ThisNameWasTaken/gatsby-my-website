13bb2749a4f857681d672ba8b836499fdcfea456
Adrian <adriandinca2008@yahoo.com>
2019-01-25 23:03:07 +0200

    chore: Add sass support

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        includePaths: [`node_modules`],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
