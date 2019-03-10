0335ef6f73b32f8de4d3e0f2906ad8e55f6a4ac6
Adrian <adriandinca2008@yahoo.com>
2019-03-10 12:53:46 +0200

    refactor: Change button's `to` attribute to `href`

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,35 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+import Button from '../components/material/Button';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO
+      title="Home"
+      keywords={[
+        'Web',
+        'Design',
+        'Development',
+        'Progressive',
+        'App',
+        'Performance',
+        'Accessibility',
+        'SEO',
+        '508 compliance',
+      ]}
+      description="This is the home page."
+    />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Button href="/page-2/">Go to page 2</Button>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import Button from '../components/material/Button';
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
+    <SEO title="Page two" description="This is the second page." />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Button href="/">Go back to the homepage</Button>
   </Layout>
-)
+);
 
-export default SecondPage
+export default SecondPage;
