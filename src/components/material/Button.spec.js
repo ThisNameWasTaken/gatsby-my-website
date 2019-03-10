d9e5ccf13390a018c660b2b66bc8c81c71708c58
Adrian <adriandinca2008@yahoo.com>
2019-03-10 12:52:22 +0200

    test: Test specific html tag attributes rendering

--- src/components/material/Button.spec.js
+++ src/components/material/Button.spec.js
@@ -1,68 +1,76 @@
 import React from 'react';
-import { mount, configure } from 'enzyme';
-import Button from './Button';
+import { mount, shallow, configure } from 'enzyme';
+import { Button } from './Button';
 import Adapter from 'enzyme-adapter-react-16';
-import { MDCRipple } from '@material/ripple';
 
 configure({ adapter: new Adapter() });
-
-jest.mock('@material/ripple', () => ({ MDCRipple: jest.fn() }));
 
 beforeEach(jest.resetAllMocks);
 
 it('adds the css classes specified by the className prop', () => {
-  const button = mount(<Button className="lorem ipsum" />).childAt(0);
+  const button = shallow(<Button className="lorem ipsum" />);
   expect(button.hasClass('mdc-button')).toBe(true);
   expect(button.hasClass('lorem')).toBe(true);
   expect(button.hasClass('ipsum')).toBe(true);
 });
 
 it('renders a raised button', () => {
-  const button = mount(<Button raised />).childAt(0);
+  const button = shallow(<Button raised />);
   expect(button.hasClass('mdc-button--raised')).toBe(true);
 });
 
 it('renders a unelevated button', () => {
-  const button = mount(<Button unelevated />).childAt(0);
+  const button = shallow(<Button unelevated />);
   expect(button.hasClass('mdc-button--unelevated')).toBe(true);
 });
 
 it('renders an outlined button', () => {
-  const button = mount(<Button outlined />).childAt(0);
+  const button = shallow(<Button outlined />);
   expect(button.hasClass('mdc-button--outlined')).toBe(true);
 });
 
 it('renders a dense button', () => {
-  const button = mount(<Button dense />).childAt(0);
+  const button = shallow(<Button dense />);
   expect(button.hasClass('mdc-button--dense')).toBe(true);
 });
 
 it('renders a button tag', () => {
-  const button = mount(<Button />).childAt(0);
+  const button = shallow(<Button />);
   expect(button.type()).toBe('button');
 });
 
+it('renders button attributes', () => {
+  const button = shallow(<Button type="submit" />);
+  expect(button.prop('type')).toBe('submit');
+});
+
 it('renders an anchor tag when a href prop is passed', () => {
-  const button = mount(<Button href='https://google.com' />).childAt(0);
+  const button = shallow(<Button href="https://google.com" />);
   expect(button.type()).toBe('a');
 });
 
-test('renders icon', () => {
-  const button = mount(<Button icon={<i className="test-icon" />} />).childAt(0);
+it('renders anchor attributes', () => {
+  const button = shallow(<Button href="https://google.com" target="_blank" />);
+  expect(button.prop('target')).toBe('_blank');
+});
+
+test('renders an icon', () => {
+  const button = shallow(<Button icon={<i className="test-icon" />} />);
+  expect(button.find('.test-icon').hasClass('mdc-button__icon')).toBe(true);
+});
+
+test('renders a trailing icon', () => {
+  const button = shallow(<Button trailingIcon={<i className="test-icon" />} />);
   expect(button.find('.test-icon').hasClass('mdc-button__icon')).toBe(true);
 });
 
 it('does not render an icon if an icon prop is not passed', () => {
-  const button = mount(<Button />).childAt(0);
+  const button = shallow(<Button />);
   expect(button.exists('.mdc-button__icon')).toBe(false);
 });
 
-it('renders ripples', () => {
-  mount(<Button />);
-  expect(MDCRipple).toHaveBeenCalled();
-});
-
-it('does not render ripples if a noRipple prop is passed', () => {
-  mount(<Button noRipple />);
-  expect(MDCRipple).not.toHaveBeenCalled();
+it('calls the init ripple function', () => {
+  const initRipple = jest.fn(() => {});
+  mount(<Button initRipple={initRipple} />);
+  expect(initRipple).toHaveBeenCalled();
 });
